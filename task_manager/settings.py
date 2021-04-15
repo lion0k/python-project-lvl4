0bd8df15e0863584ee190548525d663ea2e0a258
li0n0k <lion_ok@mail.ru>
2021-04-15 16:33:40 +0300

    try5 fix heroku 500

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'bootstrap4',
     'task_manager',
