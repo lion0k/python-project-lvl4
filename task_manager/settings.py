27310d9facf8174798470cea62b60d75b36b69cc
li0n0k <lion_ok@mail.ru>
2021-04-15 18:08:33 +0300

    I will read the task more attentively, NO static, only bootstrap

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -93,7 +92,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
@@ -107,8 +106,6 @@ LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale/'), )
 
 LANGUAGES = (('ru', 'Russian'), ('en', 'English'))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
