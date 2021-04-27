33e226258eea110acaba7fd33ce4ebe49f7b8ff6
li0n0k <lion_ok@mail.ru>
2021-04-27 18:15:09 +0300

    add CRUD, tests for model 'Labels'

---
+++ task_manager/labels/apps.py
@@ -0,0 +1,9 @@
+"""Labels application."""
+from django.apps import AppConfig
+
+
+class LabelsConfig(AppConfig):
+    """Config labels."""
+
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'task_manager.labels'
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -46,6 +46,7 @@ class UserUpdateView(
     """User update."""
 
     model = get_user_model()
+    context_object_name = 'user'
     form_class = CustomUserCreationForm
     template_name = 'users/update.html'
     success_url = reverse_lazy('login')
@@ -61,6 +62,7 @@ class UserDeleteView(
     """User delete."""
 
     model = get_user_model()
+    context_object_name = 'user'
     template_name = 'users/delete.html'
     success_url = reverse_lazy('home')
     success_message = _('SuccessDeleteUser')
